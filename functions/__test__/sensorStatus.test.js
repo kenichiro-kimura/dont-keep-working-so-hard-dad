7bcd32fa6648127114b31ef263f022c9d46cf36d
健一郎 木村 <k-kimura@alterbooth.com>
2024-06-28 08:59:03 +0900

    [modify] update status string

--- functions/__test__/sensorStatus.test.js
+++ functions/__test__/sensorStatus.test.js
@@ -9,12 +9,12 @@ describe('getStartTimeAndEndTimeOfSensorStatus', () => {
     const sensorStatusHistory = [
       { status: 'WORKING', unixtime: 1000 },
       { status: 'WORKING', unixtime: 1001 },
-      { status: 'BREAKING', unixtime: 1002 },
-      { status: 'BREAKING', unixtime: 1003 },
-      { status: 'BREAKING', unixtime: 1004 },
+      { status: 'ONBREAK', unixtime: 1002 },
+      { status: 'ONBREAK', unixtime: 1003 },
+      { status: 'ONBREAK', unixtime: 1004 },
       { status: 'WORKING', unixtime: 1005 }
     ];
-    const status = 'BREAKING';
+    const status = 'ONBREAK';
     const times = 3;
     const expected = { startTime: 1002, endTime: 1004 };
     const result = getStartTimeAndEndTimeOfSensorStatus(sensorStatusHistory, status, times, compareIsEqual);
@@ -24,12 +24,12 @@ describe('getStartTimeAndEndTimeOfSensorStatus', () => {
   test('指定されたstatusがtimes回以上続かない場合、undefinedを返す', () => {
     const sensorStatusHistory = [
       { status: 'WORKING', unixtime: 1000 },
-      { status: 'BREAKING', unixtime: 1001 },
+      { status: 'ONBREAK', unixtime: 1001 },
       { status: 'WORKING', unixtime: 1002 },
-      { status: 'BREAKING', unixtime: 1003 },
+      { status: 'ONBREAK', unixtime: 1003 },
       { status: 'WORKING', unixtime: 1004 }
     ];
-    const status = 'BREAKING';
+    const status = 'ONBREAK';
     const times = 2;
     const result = getStartTimeAndEndTimeOfSensorStatus(sensorStatusHistory, status, times, compareIsEqual);
     expect(result).toBeUndefined();
@@ -38,15 +38,15 @@ describe('getStartTimeAndEndTimeOfSensorStatus', () => {
   test('指定されたstatusがtimes回以上続くシーケンスが複数ある場合、最初に見つかったシーケンスの最初と最後のunixtimeを返す', () => {
     const sensorStatusHistory = [
       { status: 'WORKING', unixtime: 1000 },
-      { status: 'BREAKING', unixtime: 1001 },
-      { status: 'BREAKING', unixtime: 1002 },
+      { status: 'ONBREAK', unixtime: 1001 },
+      { status: 'ONBREAK', unixtime: 1002 },
       { status: 'WORKING', unixtime: 1003 },
-      { status: 'BREAKING', unixtime: 1004 },
-      { status: 'BREAKING', unixtime: 1005 },
-      { status: 'BREAKING', unixtime: 1006 },
+      { status: 'ONBREAK', unixtime: 1004 },
+      { status: 'ONBREAK', unixtime: 1005 },
+      { status: 'ONBREAK', unixtime: 1006 },
       { status: 'WORKING', unixtime: 1007 }
     ];
-    const status = 'BREAKING';
+    const status = 'ONBREAK';
     const times = 2;
     const expected = { startTime: 1001, endTime: 1002 };
     const result = getStartTimeAndEndTimeOfSensorStatus(sensorStatusHistory, status, times, compareIsEqual);
@@ -58,17 +58,17 @@ describe('getLastStartTimeAndEndTimeOfSensorStatus', () => {
   test('指定されたstatusがtimes回以上続くシーケンスが複数ある場合、最後のシーケンスの最初と最後のunixtimeを返す', () => {
     const sensorStatusHistory = [
       { status: 'WORKING', unixtime: 1000 },
-      { status: 'BREAKING', unixtime: 1001 },
-      { status: 'BREAKING', unixtime: 1002 },
-      { status: 'BREAKING', unixtime: 1003 },
+      { status: 'ONBREAK', unixtime: 1001 },
+      { status: 'ONBREAK', unixtime: 1002 },
+      { status: 'ONBREAK', unixtime: 1003 },
       { status: 'WORKING', unixtime: 1004 },
-      { status: 'BREAKING', unixtime: 1005 },
-      { status: 'BREAKING', unixtime: 1006 },
-      { status: 'BREAKING', unixtime: 1007 },
-      { status: 'BREAKING', unixtime: 1008 },
+      { status: 'ONBREAK', unixtime: 1005 },
+      { status: 'ONBREAK', unixtime: 1006 },
+      { status: 'ONBREAK', unixtime: 1007 },
+      { status: 'ONBREAK', unixtime: 1008 },
       { status: 'WORKING', unixtime: 1009 }
     ];
-    const status = 'BREAKING';
+    const status = 'ONBREAK';
     const times = 3;
     const expected = { startTime: 1005, endTime: 1008 };
     const result = getLastStartTimeAndEndTimeOfSensorStatus(sensorStatusHistory, status, times, compareIsEqual);
