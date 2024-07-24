4476d6a6d5f384d641a2f93931316104a519edfd
木村 健一郎 <k-kimura@alterbooth.com>
2024-07-24 14:25:19 +0900

    [modify] relative path must start with .

--- common-lib/index.js
+++ common-lib/index.js
@@ -1,5 +1,5 @@
-const { getNotifyTimeToTakeBreak } = require('libs/scheduleCheck');
-const { getLocalTimeStringRoundUpSeconds } = require('libs/timeFunction');
-const { getLastStartTimeAndEndTimeOfSensorStatus, getStartTimeAndEndTimeOfSensorStatus } = require('libs/sensorStatus');
+const { getNotifyTimeToTakeBreak } = require('./libs/scheduleCheck');
+const { getLocalTimeStringRoundUpSeconds } = require('./libs/timeFunction');
+const { getLastStartTimeAndEndTimeOfSensorStatus, getStartTimeAndEndTimeOfSensorStatus } = require('./libs/sensorStatus');
 
 module.exports = { getNotifyTimeToTakeBreak, getLocalTimeStringRoundUpSeconds, getLastStartTimeAndEndTimeOfSensorStatus, getStartTimeAndEndTimeOfSensorStatus };
