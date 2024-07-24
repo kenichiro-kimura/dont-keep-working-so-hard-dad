9f84f8e4465de12d622599f1d26d4466ff699133
木村 健一郎 <k-kimura@alterbooth.com>
2024-07-24 14:07:30 +0900

    [modify] move libs to common and create package

---
+++ common-lib/index.js
@@ -0,0 +1,5 @@
+const { getNotifyTimeToTakeBreak } = require('libs/scheduleCheck');
+const { getLocalTimeStringRoundUpSeconds } = require('libs/timeFunction');
+const { getLastStartTimeAndEndTimeOfSensorStatus, getStartTimeAndEndTimeOfSensorStatus } = require('libs/sensorStatus');
+
+module.exports = { getNotifyTimeToTakeBreak, getLocalTimeStringRoundUpSeconds, getLastStartTimeAndEndTimeOfSensorStatus, getStartTimeAndEndTimeOfSensorStatus };
--- functions/src/lambda-handler.js
+++ functions/src/lambda-handler.js
@@ -8,4 +8,3 @@ exports.run = async () => {
 };
 
 exports.run();
-
