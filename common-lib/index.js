0a7e649dfc111c6740b7b057febb842cf4f6c93e
木村 健一郎 <k-kimura@alterbooth.com>
2024-07-24 15:43:27 +0900

    [modify] modify logging

--- common-lib/index.js
+++ common-lib/index.js
@@ -14,6 +14,12 @@ const { getLastStartTimeAndEndTimeOfSensorStatus } = require('./libs/sensorStatu
  * @return {string} 次の休憩開始時刻
  */
 const getNextBreakTime = (sensorStatusHistory, schedules, workDuration, breakDuration, distanceThreshold, consecutiveTimes) => {
+  /**
+   * sensorStatusHistoryの中で、最後にdistanceがconsecutiveTimes回以上連続でdistanceThreshold以上になった時間を取得する
+   * sensorStatusHistoryのデータと、引き数で渡したdistanceThresholdの比較関数は a.distance >= b 。この場合のaがsensorStatusHistoryの要素、bがdistanceThresholdとなる
+   */
+  const lastBreak = getLastStartTimeAndEndTimeOfSensorStatus(sensorStatusHistory, distanceThreshold, consecutiveTimes, (a, b) => a.distance >= b);
+
   if (process.env.DEBUG === 1) {
     for (const sensorStatus of sensorStatusHistory) {
       console.log(sensorStatus);
@@ -21,14 +27,8 @@ const getNextBreakTime = (sensorStatusHistory, schedules, workDuration, breakDur
     for (const schedule of schedules) {
       console.log(schedule);
     }
+    console.log('lastBreak: ' + JSON.stringify(lastBreak));
   }
-
-  /**
-   * sensorStatusHistoryの中で、最後にdistanceがconsecutiveTimes回以上連続でdistanceThreshold以上になった時間を取得する
-   * sensorStatusHistoryのデータと、引き数で渡したdistanceThresholdの比較関数は a.distance >= b 。この場合のaがsensorStatusHistoryの要素、bがdistanceThresholdとなる
-   */
-  const lastBreak = getLastStartTimeAndEndTimeOfSensorStatus(sensorStatusHistory, distanceThreshold, consecutiveTimes, (a, b) => a.distance >= b);
-  console.log('lastBreak: ' + JSON.stringify(lastBreak));
 
   const lastBreakTime = lastBreak !== undefined ? lastBreak.endTime : 0;
   return getLocalTimeStringRoundUpSeconds(getNotifyTimeToTakeBreak(schedules, lastBreakTime, workDuration, breakDuration));
