51c5c8f27c310eb153ce0b8c350a1496fbb15174
木村 健一郎 <k-kimura@alterbooth.com>
2024-07-24 15:44:07 +0900

    [modify] write timer functions

--- lambda/lambda-handler.js
+++ lambda/lambda-handler.js
@@ -1,8 +1,20 @@
-const { getLocalTimeStringRoundUpSeconds } = require('dkwshd');
+const { getNextBreakTime } = require('dkwshd');
 
 exports.run = async () => {
-  const time = getLocalTimeStringRoundUpSeconds(new Date().getTime());
-  console.log(`Your cron function ran at ${time}`);
+  const sensorStatusHistory = [];
+  const schedules = [];
+  const distanceThreshold = process.env.distanceThreshold ? process.env.distanceThreshold : 100;
+  const consecutiveTimes = process.env.consecutiveTimes ? process.env.consecutiveTimes : 5;
+  const breakDuration = (process.env.breakDuration ? process.env.breakDuration : 300) * 1000;
+  const workDuration = (process.env.workDuration ? process.env.workDuration : 1800) * 1000;
+
+  const toNotify = getNextBreakTime(sensorStatusHistory, schedules, workDuration, breakDuration, distanceThreshold, consecutiveTimes);
+
+  console.log(`toNotify: ${toNotify}`);
+
+  /**
+   * IoTCoreにメッセージを送信
+   */
 };
 
 exports.run();
